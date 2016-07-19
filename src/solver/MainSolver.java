76d84e5021ef0087ddbca81b6c6c3653139f0c63
albertusk95 <albertusk15@gmail.com>
2016-07-19 22:38:14 +0700

    GUI

---
+++ src/solver/MainSolver.java
@@ -0,0 +1,20 @@
+
+package solver;
+
+/**
+ *
+ * @author Albertus Kelvin
+ */
+public class MainSolver {
+    
+    private char[][] matrix = new char[4][4];
+    
+    public MainSolver(char[][] matrixVal) {
+        matrix = matrixVal;
+    }
+    
+    public void solve() {
+        
+        
+    }
+}
