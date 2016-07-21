97e2269af4e91db3672e9001735effe024041373
varian97 <variancaesar@gmail.com>
2016-07-21 22:09:36 +0700

    Added Pattern Matching Algorithm

--- Wordament.cpp
+++ Wordament.cpp
@@ -1,3 +1,11 @@
+/*
+Author  : Varian Caesar
+NIM	    : 13514041
+Program : Wordament solver
+
+This program created for IRK recruitment process
+*/
+
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -39,9 +47,71 @@ void printBoard(){
 	}
 }
 
+bool findmatch(string pat, int x, int y, int level)
+{
+    if (level == pat.length()) //pattern matched
+        return true;
+        
+    if (x<0 || y<0 || x>=4 || y>=4)
+     return false; 
+    
+    if (board[x][y] == pat[level])
+    {
+        bool res;
+
+        //marking this cell as used
+        //if not found, then we continue searching from this location
+        char temp = board[x][y];
+        board[x][y] = '#';       
+             
+        //finding next char pattern in 8 neighbours     
+        res = findmatch(pat, x-1, y,level+1)   ||
+              findmatch(pat, x+1, y,level+1)   ||
+              findmatch(pat, x, y-1,level+1)   ||
+              findmatch(pat, x, y+1,level+1)   ||
+              findmatch(pat, x+1, y+1,level+1) ||
+              findmatch(pat, x+1, y-1,level+1) ||
+              findmatch(pat, x-1, y+1,level+1) ||
+              findmatch(pat, x-1, y-1,level+1);
+
+        //marking this cell as unused
+        board[x][y] = temp;
+        return res;
+    }
+    else
+        return false;
+}
+
+bool isfind(string pat)
+{
+ // if total characters in matrix is less then pattern lenghth
+    if (pat.length() > 16)
+        return false;
+
+    for (int i=0; i<4; i++) {
+    	for (int j=0; j<4; j++){
+
+      		if (board[i][j] == pat[0])
+                    if(findmatch(pat, i, j, 0)) 
+                          return true;
+     	}
+     }
+    return false;
+}
+
 int main() {
 	loadDictionary();
 	initBoard();
 	printBoard();
+	int score = 0, word_found = 0;
+	for(string pat : tab_word){
+		if(isfind(pat)){
+			cout << pat << "  " << pat.length() << endl;
+			score += pat.length();
+			word_found++;
+		}
+	}
+	cout << "Total words found : " << word_found << endl;
+	cout << "Total Score : " << score << endl;
 	return 0;
 }
