c3a8240684c42035e205f3d983f74782fd647dfd
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-07-24 01:52:02 +0700

    solving button and make it works for the first time

--- WordamentSolver/src/com/company/Main.java
+++ WordamentSolver/src/com/company/Main.java
@@ -1,8 +1,15 @@
 package com.company;
 
+import javax.swing.*;
+
 public class Main {
 
     public static void main(String[] args) {
 	    /* write your code here */
+        JFrame frame = new JFrame("Solver");
+        frame.setContentPane(new Solver().callView());
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
--- WordamentSolver/src/com/company/Solver.java
+++ WordamentSolver/src/com/company/Solver.java
@@ -1,14 +1,36 @@
 package com.company;
 
 import javax.swing.*;
-
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 /**
  * Created by raditya on 7/19/16.
  */
+
+
 public class Solver {
 
     private JButton solveButton;
     private JButton resetButton;
     private JButton exitButton;
+    private JPanel wordamentsolverView;
+
+    private class CloseListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            System.exit(0);
+        }
+    }
+
+    public Solver(){
+        exitButton.addActionListener(new CloseListener());
+    }
+
+    public JPanel callView(){
+        return wordamentsolverView;
+    }
+    /* kalo mau ngapa ngapain itu harus didalem kelasnya */
+
 
 }
+
