79f4a6b48f2a862617cfc7a23f893700444656e6
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-07-23 14:27:08 +0700

    Added form and button

---
+++ WordamentSolver/src/com/company/Solver.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import javax.swing.*;
+
+/**
+ * Created by raditya on 7/19/16.
+ */
+public class Solver {
+
+    private JButton solveButton;
+    private JButton resetButton;
+    private JButton exitButton;
+
+}
