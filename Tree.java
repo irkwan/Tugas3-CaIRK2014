7927c59a561cf32ecf383d3a30dd461c32d40860
MalvinJu <malvinjuanda@yahoo.com>
2016-07-24 19:54:43 +0700

    Add files via upload
    Struktur data tree dan pembacaan dictionary

---
+++ Tree.java
@@ -0,0 +1,68 @@
+
+public class Tree{
+        Tree[] node;
+        int count;
+        Tree(){
+            count = 0;
+        }
+        /**
+         * @param s a non-null string.
+        **/
+        Tree(String s) {
+            if(s.length()==0){
+                count = 1;
+                return;
+            } else {
+                node = new Tree[26];
+                node[s.charAt(0)-'a'] = new Tree(s.substring(1));
+            }
+        }
+        /**
+         * @param s a non-null string.
+        **/        
+        void addWord(String s) {
+            if(s.length()==0){
+                count++;
+            }
+            else if (node == null){
+                node = new Tree[26];
+                node[s.charAt(0)-'a'] = new Tree(s.substring(1));
+            }else{
+                if (node[s.charAt(0)-'a'] == null)
+                {
+                    node[s.charAt(0)-'a'] = new Tree(s.substring(1));  
+                }
+                else {
+                    node[s.charAt(0)-'a'].addWord(s.substring(1));
+                }
+            }
+        }
+
+        Tree getNode(char c) {
+            int i = c - 'a';
+            return node[i];
+        }
+
+        Boolean isWord() {
+           if (count == 1) {
+                return true;
+           }
+           else{
+                return false;
+           }
+        }
+
+        public static void main (String[] args) {
+            Tree coba = new Tree();
+            coba.addWord("caca");
+            coba.addWord("cada");
+            coba.addWord("sapi");
+
+            
+            System.out.print(coba.getNode('c').getNode('a').getNode('c').getNode('a').count);
+            System.out.print(coba.getNode('c').getNode('a').getNode('d').getNode('a'));
+            System.out.print(coba.getNode('s').getNode('a').getNode('d'));
+            System.out.print(coba.getNode('s').getNode('a').getNode('p').getNode('i').count);
+        }
+    }
+
