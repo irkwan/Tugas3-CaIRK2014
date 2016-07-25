bf1d336cc1f1c808acee816ab0508de8174db713
Alfonsus Raditya Arsadjaja <raditya1710@gmail.com>
2016-07-25 12:57:20 +0700

    Added algorithm and data structure

---
+++ WordamentSolver/res/Trie.java
@@ -0,0 +1,35 @@
+public class Trie{
+	private char ch;
+	private Trie[] next;
+	private boolean exists;
+
+	public Trie(){
+		ch = '\0';
+		exists = false;
+		next = new Trie[26];
+	}
+	public Trie(int idx){
+		ch = (char) ('a' + idx);
+		exists = false;
+		next = new Trie[26];
+	}
+	public char getChar(){
+		return ch;
+	}
+
+	public Trie getNext(int idx){
+		return next[idx];
+	}
+
+	public void setNext(int idx){
+		next[idx] = new Trie(idx);
+	}
+
+	public void setExist(){
+		exists = true;
+	}
+
+	public boolean getExist(){
+		return exists;
+	}
+}
---
+++ WordamentSolver/res/Wordament.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+import java.util.Set;
+import java.util.HashSet;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.Collections;
+
+public class Wordament{
+
+	public static ArrayList<String> res;
+	public static Trie t;
+
+	private static char[][] m;
+	private final static int size = 4;
+	private static boolean[][] visited;
+	private static int[] arahX;
+	private static int[] arahY;
+	
+	private static boolean valid(int x, int y){
+		return x >= 0 && y >= 0 && x < size && y < size && !visited[x][y];
+	}
+
+	private static void recurse(int last,String s, Trie tNow){
+		if(tNow.getExist()){
+			res.add(s);
+		}
+		int x = last / size;
+		int y = last % size;
+		for(int i = 0;i < 8; ++i){
+			int nowX = x + arahX[i];
+			int nowY = y + arahY[i];
+			if(valid(nowX, nowY)){
+				visited[nowX][nowY] = true;
+				int idx = (int)m[nowX][nowY] - 97;
+				if(tNow.getNext(idx) != null){
+					recurse(nowX * size + nowY, s + Character.toString(m[nowX][nowY]), tNow.getNext(idx));
+				}
+				visited[nowX][nowY] = false;
+			}
+		}
+	}
+
+	public static void Init(){
+		visited = new boolean[size][size];
+		arahX = new int[]{-1,-1,0,1,1,1,0,-1};
+		arahY = new int[]{0,-1,-1,-1,0,1,1,1};
+		t = new Trie();
+		m = new char[size][size];
+		// still in progress
+	}
+	public static void InitDictionary() throws IOException{
+		BufferedReader br = new BufferedReader(new FileReader("wordsEn.txt"));
+		String sNow;
+		while((sNow = br.readLine()) != null){
+			int len = sNow.length();
+			Trie tNow = t;
+			for(int i = 0;i < len; ++i){
+				int ch = (int)sNow.charAt(i) - 97;
+				if(tNow.getNext(ch) == null){
+					tNow.setNext(ch);
+				}
+				tNow = tNow.getNext(ch);
+			}	
+			tNow.setExist();
+		}
+		br.close();
+	}
+	public static ArrayList<String> Solve(char[][] _m){
+		res = new ArrayList<>();
+		for(int i = 0;i < size; ++i){
+			for(int j = 0;j < size;++j){
+				m[i][j] = _m[i][j];
+			}
+		}
+		for(int i = 0;i < size; ++i){
+			for(int j = 0;j < size; ++j){
+				visited[i][j] = true;
+				int idx = (int)m[i][j] - 97;
+
+				if(t.getNext(idx) != null){
+					recurse(i * size + j, Character.toString(m[i][j]), t.getNext(idx));
+				}
+				
+				visited[i][j] = false;
+			}
+		}
+		Set<String> hs = new HashSet<>();
+		hs.addAll(res);
+		res.clear();
+		res.addAll(hs);
+		Collections.sort(res);
+		return res;
+	}
+}
